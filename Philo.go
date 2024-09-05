38f74c593da3d364d2718c41fd657fc1cc528b9f
Nikolaj <nals@itu.dk>
2024-09-05 15:53:07 +0200

    Begang fleshing out logic from comments
    We chose to not use sync.mutex locks as we found it unnecessary as philosopher and forks *only* can communicate via channels.
    
    Co-Authored-By: Nikolaj787 <70134368+Nikolaj787@users.noreply.github.com>
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- Philo.go
+++ Philo.go
@@ -7,20 +7,27 @@ import (
 )
 
 func main() {
-
-	forks := make([]Fork, 5)
-	for index, f := range forks {
-		f.id = index+1;
+	var wg sync.WaitGroup
+	philoCount := 1
+	forks := make([]Fork, 2)
+	for index := range forks {
+		forks[index].occupationCh = make(chan int)
 	}
 
-	philosophers := make([]Philosopher, 5)
-	for index, p := range philosophers {
-		p.id = index+1;
-		p.Rightfork = forks[index+1]
-		p.Leftfork = forks[index+2]
+	philosophers := make([]Philosopher, 1)
+	for index := range philosophers {
+		philosophers[index].id = index
+		philosophers[index].eatCount = 0
+		//p.Rightfork = forks[index]
+		//p.Leftfork = forks[index+1]
+		fmt.Print("Eat is about to be called")
+		go philosophers[index].Eat(&forks[index], &forks[(index+1)%philoCount])
+		// Wait here
+		fmt.Print("\nP has eaten this much: ", philosophers[index].eatCount)
 	}
+	wg.Wait()
+	fmt.Print("\nFinish")
 
-	// var mu1,mu2,mu3,mu4,mu5 sync.Mutex
 }
 
 // 1. Each philosopher must eat 3 times
@@ -33,36 +40,81 @@ func main() {
 // must communicate through channels
 
 // Possible goroutine is that they lock themselves when communicated frm philosopher
+// Possible ways to avoid deadlock could be randomizing intervals or saying that all but one philosopher attempt to pick up right fork and one picks up left
 
-// eat lock forks
-func Eat(philo Philosopher) {
-	// receive forks from channel
-	// wait for forks are ready
-	// lock forks
-	philo.eatCount++
-	fmt.Println("Philosopher", philo.id, "is eating", "he has eaten", philo.eatCount, "times")
-	// unlock forks
-	// send them in channel
+type Philosopher struct {
+	id       int
+	eatCount int
+	// LeftFork  chan Fork // Use a channel to request the left fork
+	// RightFork chan Fork // Use a channel to request the right fork
 }
 
-// think Fjerne lock fork
-func think(philo Philosopher) { // think delay 1-2
-
+func (philo *Philosopher) think(wg *sync.WaitGroup) { // think delay 1-2
 	// wait for forks are ready
 	// receive from channel whether fork is being used by check whether it is locked?
 	// recieve fork here from channel lock funch EAT
 
+	defer wg.Done()
 	fmt.Println("Philosopher", philo.id, "is thinking")
+	time.Sleep(5 * time.Second)
+
 }
 
-type Philosopher() struct {
-	id        int
-	eatCount  int
-	Leftfork  chan bool
-	Rightfork chan bool
+func (philo *Philosopher) Eat(Fork1 *Fork, Fork2 *Fork) {
+	// receive forks from channel
+	// lock forks
+	Fork1.occupationCh <- 1
+	Fork2.occupationCh <- 1
+
+	fmt.Print("Before fork go routines")
+	go Fork1.UseFork()
+	go Fork2.UseFork()
+	fmt.Print("After fork go routines")
+
+	forkOcuppationOne := <-Fork1.occupationCh
+	forkOcuppationTwo := <-Fork2.occupationCh
+
+	if forkOcuppationOne == 0 && forkOcuppationTwo == 0 {
+		philo.eatCount++
+		fmt.Println("Philosopher", philo.id, "is eating", "he has eaten", philo.eatCount, "times")
+	}
+	// unlock forks
+	// send them in channel
+
+	// He has eaten
+	Fork1.occupationCh <- 0
+	Fork2.occupationCh <- 0
+
+	philo.think(&sync.WaitGroup{})
 }
 
 type Fork struct {
-	id   int
-	lock sync.Mutex
+	id           int
+	isOcuppied   int
+	occupationCh chan int // 0 = unoccupied, 1 == occupied
+	//lock sync.Mutex
+}
+
+func (fork *Fork) UseFork() {
+	var order int
+	// State stores 0 or 1 depending on if the fork is currently occupied
+	fmt.Print("UseFork() was called")
+
+	order = <-fork.occupationCh // philosopher has sent out either a 0 or 1 - 0 = placed fork down, 1 = want to pick up
+
+	switch order {
+	case 0:
+		// if 0 philosopher drops the fork, and state of fork is changed to 0
+		fork.isOcuppied = 0
+	case 1:
+		// if fork is occupied, send value 1 into the channel to let the philosopher know
+		if fork.isOcuppied == 1 {
+			fork.occupationCh <- 1 // Send a value from channel
+		} else {
+			// if fork isn't occupied, send value 0 into the channel to let the philosopher know
+			fork.occupationCh <- 0 // Send a value from channel
+			fork.isOcuppied = 1
+		}
+
+	}
 }
