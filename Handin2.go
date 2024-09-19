0eb028c8bdea5520c3174b8d7638ab70108f0ee2
Nikolaj Carlson <nhca@itu.dk>
2024-09-19 11:06:59 +0200

    Handshake done using goRoutines
    Co-Authored-By: Mathias Niklas Larsen <143803020+DragonFlyersx@users.noreply.github.com>
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>
    Co-Authored-By: Nikolaj <69769473+niko391a@users.noreply.github.com>

--- Handin2.go
+++ Handin2.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-const clientAmount = 1
+var wg sync.WaitGroup
 
 func main() {
+
 	// Implement the TCP/IP Handshake using threads. This is not realistic (since the protocol should run across a network)
 	//but your implementation needs to show that you have a good understanding of the protocol.
-	clients := make([]Client, clientAmount)
-	for index := range clients {
-		clients[index] = Client{
-			sequenceNumber: index * 100,
-			ackChannel:     make(chan int, 1),
-			seqChannel:     make(chan int, 1),
-		}
+	client := Client{
+		sequenceNumber: 100,
+		// ackChannel:     make(chan int, 1),
+		// seqChannel:     make(chan int, 1),
+
 	}
+
 	server := Server{
 		sequenceNumber: 300,
 		ackChannel:     make(chan int, 64000),
 		seqChannel:     make(chan int, 64000),
 	}
 
-	// hi Send
-	//ack sender tilbage recieve
-	// hi sender tilbagge send
-	// connection estabilished
+	wg.Add(2)
+
+	go StartServer(&server)
+
+	go StartClient(&client, &server)
+
+	wg.Wait()
+
 }
 
 type Server struct {
@@ -37,31 +42,67 @@ type Server struct {
 
 type Client struct {
 	sequenceNumber int
-	ackChannel     chan int
-	seqChannel     chan int
+	// ackChannel     chan int
+	// seqChannel     chan int
 }
 
-func SYN(client *Client) {
+func StartClient(client *Client, server *Server) {
+	defer wg.Done()
+	fmt.Println("Client Established")
+
 	// send client sequence number
-	client.seqChannel <- client.sequenceNumber
-}
-func ACK(client *Client) {
-	// recieve
-	var recieved = <-client.seqChannel
+	server.seqChannel <- client.sequenceNumber
+	fmt.Printf("%s %d\n", "The client has sent the sequence number: ", client.sequenceNumber)
 
-	// send
-	client.seqChannel <- recieved
-	client.ackChannel <- recieved + 1
-}
+	// recieve back it's own sequence number incremented
+	var recievedACK = <-server.ackChannel
+	// receive the servers sequence number
+	var recievedSEQ = <-server.seqChannel
+	if recievedACK == client.sequenceNumber+1 {
+		fmt.Printf("%s %d %s\n", "The client has received the correct sequence number: ", recievedACK, " from server")
 
-func SYNACK(server *Server) {
-	for {
-		// Receive
-		var recieved = <-server.seqChannel
-
-		// Send receive number + 1 and it's own sequence number
-		server.seqChannel <- recieved + 1
-		server.ackChannel <- server.sequenceNumber
-		fmt.Println("Server recieved SYN")
+	} else {
+		// Handshake failed
+		fmt.Printf("%s %d %s\n", "The client has received the wrong sequence number: ", recievedACK, " from server")
+		fmt.Printf("%s %d %s\n", "The client will loop back and restart the handshake")
+		// If client receives wrong sequence number, loop back and start handshake over again
+		//quit <- true
 	}
+
+	// send servers sequence number back incremented
+	server.ackChannel <- recievedSEQ + 1
+	// send the clients own incremented sequence number back
+	server.seqChannel <- recievedACK
+	//fmt.Printf("%s %d %s %d\n", "Client has finalized the handshake using the client sequence number: ", recievedACK, " and the server sequence number ", recievedSEQ)
 }
+
+func StartServer(server *Server) {
+	defer wg.Done()
+	fmt.Println("Server Established")
+
+	// Receive
+	var recieved = <-server.seqChannel // Receive sequence number from client
+
+	// Send
+	server.ackChannel <- recieved + 1          // Sends client sequence number back incremented by 1
+	server.seqChannel <- server.sequenceNumber // Sends servers sequence number back
+
+	fmt.Println("Server recieved SYN")
+
+	// Recieve
+	var recievedACK = <-server.ackChannel
+	recieved = <-server.seqChannel
+
+	if recievedACK == server.sequenceNumber+1 {
+		fmt.Println("Server has finalized the handshake using the client sequence number: ", recieved, " and the server sequence number ", recievedACK)
+		fmt.Println("Connection Establish")
+	} else {
+		fmt.Println("Connection Failed")
+	}
+	// Send receive number + 1 and it's own sequence number
+}
+
+// 1 channel til server seq og en channel til client
+// først sender client sit id
+//når serveren modtager incrementer den client id med 1 og sender server id tilbage
+// client incrementer server og sender tilbage
